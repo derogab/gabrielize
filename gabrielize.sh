7a943fa746dd65c1a0c189b7877642ca2af4680e
derogab <derosagabriele@outlook.it>
2019-08-16 14:46:25 +0200

    Wallpaper

--- gabrielize.sh
+++ gabrielize.sh
@@ -19,3 +19,14 @@ yay -Syu
 # Plank is meant to be the simplest dock on the planet.
 # https://github.com/ricotz/plank
 yay -S plank
+
+# Wallpaper
+# Set a wonderful wallpaper 
+# https://gitlab.manjaro.org/artwork/wallpapers/wallpapers-2018/raw/master/nature-3058859.jpg
+# and also in /usr/share/backgrounds/wallpapers-2018/
+wget -O /tmp/wallpaper.jpg https://gitlab.manjaro.org/artwork/wallpapers/wallpapers-2018/raw/master/nature-3058859.jpg
+cp /tmp/wallpaper.jpg $HOME/.wallpaper.jpg
+for property in $(xfconf-query -c xfce4-desktop -l | grep "last-image$")
+do
+    xfconf-query -c xfce4-desktop -p $property -s $HOME/.wallpaper.jpg
+done
