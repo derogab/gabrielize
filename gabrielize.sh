097b075ececb999f97f5a8cad105f216c1c7a630
derogab <derosagabriele@outlook.it>
2019-08-16 13:19:01 +0200

    Yay

---
+++ gabrielize.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+### GABRIELIZE ###
+
+## Install & configure
+
+# Yay
+# Yet Another Yogurt - An AUR Helper Written in Go
+# https://github.com/Jguer/yay 
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si
+
